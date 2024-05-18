bba0fa66bbf0d0c152eb159e284ef3f17b769782
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-18 01:47:35 -0300

    :wastebasket: remove: Removendo arquivos de atividades
    Iniciar tudo do início com um clean code melhor

--- Temporada 01 - Introdução ao Javascript/Ep 02 - Escopo e Estruturas de Controle/Atividades/AT07 - Robô da Tabuada/script.js
+++
@@ -1,8 +0,0 @@
-const numero = parseFloat(window.prompt("De qual número você deseja calcular a tabuada?"))
-let resultado = ""
-
-for (let fator = 1; fator <= 20; fator++){
-    resultado += " -> " + numero + " x " + fator + " = " + (numero * fator) + "\n"
-}
-
-alert("Resultado da Tabuada de " + numero + ":\n\n" + resultado)
