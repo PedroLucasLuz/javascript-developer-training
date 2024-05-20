2877320325c753d094eae7a3ca5e9399c3592b82
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-20 00:56:26 -0300

    :sparkles: feat: Nova Atividade
    Registro de Alunos

---
+++ Temporada 01 - Introdução ao Javascript/Ep 02 - Funções/Atividades/AT05_Registro-de-Alunos.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function prompt(question){
+    return new Promise(resposta => leitor.question(question, resposta));
+}
+
+async function obterDadosDoAluno(){
+    const $nomeDoAluno = await prompt('Informe o nome do aluno: ');
+    const $idadeDoAluno = parseInt(await prompt('Informe a idade do aluno: '));
+    if (isNaN($idadeDoAluno ) || $idadeDoAluno  < 0) throw new Error('Idade inválida');
+
+    const $serie = await prompt('Informe a série do aluno: ');
+    const $turno = await prompt('Informe o turno do aluno (manhã/tarde): ');
+
+    return {$nomeDoAluno, $idadeDoAluno, $serie, $turno}
+}
+
+function exibirDados({$nomeDoAluno, $idadeDoAluno, $serie, $turno}){
+    console.log(`
+       -- Dados do Aluno --
+       Nome: ${$nomeDoAluno}
+       Idade: ${$idadeDoAluno}
+       Série: ${$serie}
+       Turno: ${$turno}
+    `)
+}
+
+async function main(){
+    try {
+        const $dadosDoAluno = await obterDadosDoAluno();
+        exibirDados($dadosDoAluno);
+    } catch (error) {
+        console.error(error.message);
+    }finally{
+        leitor.close();
+    }
+}
+
+main();
