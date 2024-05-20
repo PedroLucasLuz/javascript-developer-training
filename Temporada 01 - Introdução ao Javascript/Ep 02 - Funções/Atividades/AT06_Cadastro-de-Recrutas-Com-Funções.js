e3bb4cb61abc46930a85d530b655a153e0f14621
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-20 16:48:04 -0300

    :sparkles: feat: Nova Atividade
    Cadastro de Recrutas Com Funções

---
+++ Temporada 01 - Introdução ao Javascript/Ep 02 - Funções/Atividades/AT06_Cadastro-de-Recrutas-Com-Funções.js
@@ -0,0 +1,67 @@
+// Importa o módulo readline para interagir com a entrada e saída do terminal
+const readline = require('readline');
+
+// Obtém o ano atual
+const $anoAtual = new Date().getFullYear();
+
+// Cria uma interface de leitura para interagir com o terminal
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Função assíncrona para exibir uma pergunta no terminal e aguardar a resposta do usuário
+function prompt(question) {
+    return new Promise(resposta => leitor.question(question, resposta));
+}
+
+// Função assíncrona para coletar os dados do recruta a partir do terminal
+async function obterEntradaTerminal() {
+    // Pergunta ao usuário e aguarda a entrada para cada campo de dados do recruta
+    const $primeiroNome = await prompt('Digite o primeiro nome do recruta: ');
+    const $sobrenome = await prompt('Digite o sobrenome do recruta: ');
+    const $campoDeEstudo = await prompt('Digite o campo de estudo do recruta: ');
+    const $anoDeNascimento = parseInt(await prompt("Digite o ano de nascimento do recruta: "));
+
+    // Retorna um objeto contendo os dados coletados do recruta
+    return { $primeiroNome, $sobrenome, $campoDeEstudo, $anoDeNascimento };
+}
+
+// Função para calcular a idade do recruta
+const $idade = ($anoAtual, $anoDeNascimento) => $anoAtual - $anoDeNascimento;
+
+// Função para construir o nome completo do recruta
+const $nomeCompleto = ($primeiroNome, $sobrenome) => `${$primeiroNome} ${$sobrenome}`;
+
+// Função para exibir os dados do recruta no terminal
+function exibirDados(recruta) {
+    // Calcula a idade do recruta
+    const idade = $idade($anoAtual, recruta.$anoDeNascimento);
+    // Constrói o nome completo do recruta
+    const nome = $nomeCompleto(recruta.$primeiroNome, recruta.$sobrenome);
+    // Exibe os dados formatados no terminal
+    console.log(`
+        Nome Completo: ${nome}
+        Campo de Estudo: ${recruta.$campoDeEstudo}
+        Idade: ${idade}
+    `);
+}
+
+// Função principal assíncrona para coordenar a execução do programa
+async function main() {
+    try {
+        // Obtém os dados do recruta
+        const recruta = await obterEntradaTerminal();
+        // Exibe os dados do recruta
+        exibirDados(recruta);
+    } catch (error) {
+        // Exibe mensagens de erro se ocorrerem durante a execução
+        console.error(error.message);
+    } finally {
+        // Fecha a interface de leitura após a execução
+        leitor.close();
+    }
+}
+
+// Chama a função principal para iniciar o programa
+main();
