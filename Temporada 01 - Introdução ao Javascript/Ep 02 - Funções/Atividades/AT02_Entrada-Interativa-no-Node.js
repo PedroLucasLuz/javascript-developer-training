a837b7351bfd78bf61a421078fb938752cd15f2f
Pedro Lucas L. Costa <pedro.lucas.luz.dev@gmail.com>
2024-05-19 22:07:55 -0300

    :sparkles: feat: Nova Atividade
    Entrada Interativa no NodeJs

---
+++ Temporada 01 - Introdução ao Javascript/Ep 02 - Funções/Atividades/AT02_Entrada-Interativa-no-Node.js
@@ -0,0 +1,28 @@
+// Importação do módulo readline
+const readline = require('readline');
+
+// Criação da interface readline
+const leitor = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let $primeiroNome, $sobrenome, $nomeCompleto;
+
+leitor.question('Digite o primeiro nome: ', (resposta) => {
+    $primeiroNome = resposta;
+    leitor.question('Digite o sobrenome: ', (resposta) => {
+      $sobrenome = resposta;
+      // Após obter os dados do usuário, exibe a saudação com o nome completo
+      console.log(`
+        Olá, meu nome é ${exibirNomeCompleto($primeiroNome, $sobrenome)}
+      `);
+      // Fecha a interface readline após exibir a saudação
+      leitor.close();
+    });
+});
+
+function exibirNomeCompleto($primeiroNome, $sobrenome) {
+    $nomeCompleto = $primeiroNome + " " + $sobrenome;
+    return $nomeCompleto;
+}
